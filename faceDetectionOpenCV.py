f7ce5bc62337c7a3bac8caa5984f63e91477159d
josenri <josprimen@alum.us.es>
2020-08-26 23:46:41 +0200

    print solution in original image code added

--- faceDetectionOpenCV.py
+++ faceDetectionOpenCV.py
@@ -1,16 +1,34 @@
 import cv2
 from cv2 import CascadeClassifier
 from cv2 import imread
+from cv2 import rectangle
+from cv2 import imshow
+from cv2 import waitKey
+from cv2 import destroyAllWindows
 #print(cv2.__version__)
 
 #Cargamos un modelo pre-entrenado para reconocimiento facial y creamos un modelo en cascada con el
 classifier = CascadeClassifier('haarcascade_frontalface_default.xml')
 
 #cargamos una foto de prueba
-pixels = imread('test1.jpg')
+pixels = imread('test2.jpg')
 
 #realizamos el reconocimiento facial
 bboxes = classifier.detectMultiScale(pixels)
 #representamos los cuadros delimitadores de los rostros detectados
 for box in bboxes:
     print(box)
+    # para mostrar la imagen original con los cuadros delimitadores dibujados encima haremos lo siguiente:
+    # extraemos cada propiedad del resultado (x e y de la esquina inferior izq del cuadro delimitador y su altura y anchura)
+    x, y, width, height = box
+    x2, y2 = x + width, y + height
+    # dibujamos un rectangulo sobre los pixels
+    rectangle(pixels, (x, y), (x2, y2), (0, 0, 255), 1)
+
+#imprimimos la imagen con la deteccion y la mantenamos abierta hasta que el usuario pulse una tecla
+#mostramos la imagen
+imshow('face detection', pixels)
+#mantenemos la ventana abierta hasta que se pulse una tecla
+waitKey(0)
+#cerramos la ventana
+destroyAllWindows()
