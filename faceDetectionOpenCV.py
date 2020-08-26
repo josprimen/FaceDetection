9bad9d9ff1cd4eda31192dc536549fb9fc2a1477
josenri <josprimen@alum.us.es>
2020-08-26 21:52:06 +0200

    code to create a cascade model with pretrainded face detection model and run it with an example

--- faceDetectionOpenCV.py
+++ faceDetectionOpenCV.py
@@ -1,2 +1,16 @@
 import cv2
-print(cv2.__version__)
+from cv2 import CascadeClassifier
+from cv2 import imread
+#print(cv2.__version__)
+
+#Cargamos un modelo pre-entrenado para reconocimiento facial y creamos un modelo en cascada con el
+classifier = CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#cargamos una foto de prueba
+pixels = imread('test1.jpg')
+
+#realizamos el reconocimiento facial
+bboxes = classifier.detectMultiScale(pixels)
+#representamos los cuadros delimitadores de los rostros detectados
+for box in bboxes:
+    print(box)
