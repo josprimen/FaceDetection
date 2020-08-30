f272076b7dc79d4a22513061b1a33b41b28c5981
josenri <josprimen@alum.us.es>
2020-08-30 13:48:26 +0200

    final code, draw result faces function added

--- faceDetectionDeepLearning.py
+++ faceDetectionDeepLearning.py
@@ -32,6 +32,21 @@ def draw_image_with_boxes(filename, result_list):
     pyplot.show()
 
 
+def draw_result_faces(filename, result_list):
+    #cargamos la imagen
+    data = pyplot.imread(filename)
+    for i in range(len(result_list)):
+        #obtener las coordenadas
+        x, y, width, height = result_list[i]['box']
+        x2, y2 = x + width, y + height
+        #los parametros de subplot son filas, columnas e indice
+        pyplot.subplot(1, len(result_list), i+1)
+        pyplot.axis('off')
+        pyplot.imshow(data[y:y2, x:x2])
+    #mostramos por pantalla
+    pyplot.show()
+
+
 
 filename = 'image/foto grupal.jpg'
 #Usamos la funcion imread de pyplot en lugar de la de openv
@@ -43,6 +58,8 @@ faces = detector.detect_faces(pixels)
 for face in faces:
     print(face)
 #usamos una función para mostrar todos los datos por pantalla, tenemos que pasar la imagen original y las caras detectadas
-draw_image_with_boxes(filename, faces)
+#draw_image_with_boxes(filename, faces)
+#tambien podemos crear una funcion para solo mostrar los rostros detectados
+draw_result_faces(filename, faces)
 
 
